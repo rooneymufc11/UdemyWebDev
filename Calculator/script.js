0798fa6c7e47a44296d60c16ae1feeb4a820d532
Manasi <manasi.n.p1110@gmail.com>
2017-07-04 00:17:20 -0400

    almost complete js

--- Calculator/script.js
+++ Calculator/script.js
@@ -4,11 +4,15 @@ var decimal = document.querySelector("#decimal");
 var equals = document.querySelector("#equal");
 var screen = document.querySelector("#screen");
 var result;
+var ipCheck;
+var prev;
 var current = [];
 var op = [];
+var number = [];
 var sign = false;
 var dflag = false;
 var eqflag = false;
+
 
 
 decimal.addEventListener("click", function(){
@@ -28,49 +32,95 @@ decimal.addEventListener("click", function(){
 });
 
 for(var i=0; i<num.length; i++){
-	num[i].addEventListener("click", function(){
-		if(sign){
+		num[i].addEventListener("click", function(){
+		if(eqflag){
+		eqflag=false;
+		result=undefined;
+		screen.innerHTML = this.textContent;
+		}
+		else if(sign){
 			screen.innerHTML = this.textContent;
 			sign=false;
 		}
+		else if(current[current.length-1] == '-'&&current.length==1){
+			screen.innerHTML = current[current.length-1] + this.textContent;
+		}
+
 		else{
 			screen.innerHTML += this.textContent;
 		}
 	current.push(this.textContent);
+	number.push(this.textContent);
+	ipCheck = number.join('');
+	if(checkBits(ipCheck)){
+		screen.innerHTML = "Error";
+	}
 });
 }
 
 
 for(var i=0; i<operators.length; i++){
 	operators[i].addEventListener("click", function(){
+		number = [];
 		if(eqflag){
 			current.push(result);
 			eqflag = false;
 		}
-
 		sign = true;
 		dflag=false;
+
 		 if(this.textContent == '÷'){
-				if(op[op.length-1] == '*' || op[op.length-1] == '/'){
-				screen.innerHTML = eval(current.join(''));
-			}
-			current.push('/')
-			op.push('/');
-		}
+		 		result = firstOp(current,'/',op);
+		 	}
+			// 	if(op[op.length-1] == '*' || op[op.length-1] == '/'){
+			// 		// result = eval(current.join(''));
+			// 		result = calculate(current);
+			// 		// screen.innerHTML = result;
+			// 	 //screen.innerHTML = eval(current.join(''));
+			// 	 }
+			// 	 else if(op.length==0){
+			// 	 	console.log("i am empty");
+			// 	 }
+			// current.push('/')
+			// op.push('/');
 		
-		else if(this.textContent == 'x' ){
-				if(op[op.length-1] == '*' || op[op.length-1] == '/'){
-				screen.innerHTML = eval(current.join(''));
+		
+		else if(this.textContent == 'x'){
+				result = firstOp(current,'*',op);
 			}
-			current.push('*');
-			op.push('*');
+			// 	if(op[op.length-1] == '*' || op[op.length-1] == '/'){
+			// 	// screen.innerHTML = eval(current.join(''));
+			// 	// result = eval(current.join(''));
+			// 	 result = calculate(current);
+			// 	 // screen.innerHTML = result;
+				 
+			// }
+			// current.push('*');
+			// op.push('*');
+
+		else if(this.textContent == '+'){
+			result = firstOp(current,'+',op);
 		}
 
 		else{
-			
-			screen.innerHTML = eval(current.join(''));
-			current.push(this.textContent);
-		}
+				if(current.length==0){
+				screen.innerHTML = '-';
+				current.push('-');
+				sign = false;
+				}
+				else{
+				result = firstOp(current,'-',op);
+				}
+			}
+			// 			result = calculate(current);
+			// 	// screen.innerHTML = result;
+			//  // screen.innerHTML = eval(current.join(''));
+			//  console.log("inside of + - "+current);
+			// current.push(this.textContent);
+			// op.push(this.textContent);
+					
+				
+				
 	});
 }
 
@@ -78,32 +128,110 @@ equals.addEventListener("click", function(){
 	if(eqflag){
 		screen.innerHTML;
 	}
-
+	
+	else if(op[op.length-1] == '/' && current[current.length-1] == '0'){
+			screen.innerHTML = 'undefined';
+			result = undefined;
+			current = [];
+			eqflag = true;
+	}
+	
 	else{
 	eqflag = true;
-	console.log(eqflag);
-	sign = true;
+	// sign = true;
 	dflag=false;
-	console.log(current.join(''));
-	result=eval(current.join(''));
-	if((Math.max(Math.floor(Math.log10(Math.abs(result))), 0) + 1)>10){
-		screen.innerHTML = 'Error';
-		current = [];
-		var result;
-	}
-	else if(result.toString().indexOf('.')!=-1){
-		screen.innerHTML = result.toPrecision(10);
-		result = result.toPrecision(10);
-		console.log(result);
-		current = [];
-	}
+	// console.log(current.join(''));
+	// result=eval(current.join(''));
+	// console.log("outside "+ result);
+	result = calculate(current);
+	current = [];
 
-	else{
-		screen.innerHTML = result;
-		current = [];
-	}
+		// if((Math.max(Math.floor(Math.log10(Math.abs(result))), 0) + 1)>10){
+		// screen.innerHTML = 'Error';
+		// current = [];
+		// result = undefined;
+		// // eqflag = false;
+		// }
+
+		// else if(result.toString().indexOf('.')!=-1){
+		// 	screen.innerHTML = result.toPrecision(10);
+		// 	result = result.toPrecision(10);
+		// 	console.log(result);
+		// 	current = [];
+		// 	console.log("inside decimal" + result);
+		// }
+
+
+		// else {
+		// 	screen.innerHTML = result;
+		// 	current = [];
+		// 	console.log("ekdum normal" + result);
+		// 	}
 	
 	}
 });
 
+function calculate(current){
+	// eqflag = true;
+	// console.log(eqflag);
+	// // sign = true;
+	// dflag=false;
+	result=eval(current.join(''));
+	// if((Math.max(Math.floor(Math.log10(Math.abs(result))), 0) + 1)>10){
+	// 		screen.innerHTML = 'Error';
+	// 		result = undefined;
+	// 		return result;
+	// 	}
 
+		if(checkBits(result)){
+			screen.innerHTML = "Error";
+			return result;
+		}
+
+		else if(result.toString().indexOf('.')!=-1){
+			// screen.innerHTML = result.toPrecision(10);
+			screen.innerHTML = +(+result.toPrecision(10)).toFixed(10);
+			result = result.toPrecision(10);
+			return result;
+		}
+
+
+		else {
+			screen.innerHTML = result;
+			return result;
+			}
+}
+
+function checkBits(x){
+	if((Math.max(Math.floor(Math.log10(Math.abs(x))), 0) + 1)>10){
+			result = undefined;
+			return true;
+		}
+	else return false;
+}
+
+function firstOp(current,operator,op){
+		prev = op[op.length-1];
+		if(current.length==0){
+			
+			op = [];
+			current = [];
+			return undefined;
+		}
+
+		else if(prev == '*' || prev == '/'|| op.length==0 || (prev=='+' && operator=='+') || (prev=='-' && operator=='-')||(prev=='+' && operator=='-')||(prev=='-' && operator=='+')){
+			
+			result = calculate(current);
+			current.push(operator);
+			op.push(operator);
+			return result;
+		}
+
+		else if(prev == '+'|| prev == '-'){
+			console.log("third + and -");
+			current.push(operator);
+			op.push(operator);
+			return result;
+	}
+
+}
